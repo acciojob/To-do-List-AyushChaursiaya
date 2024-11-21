534616145ff75ea2b979535a269ab19a4808c247
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-11-21 20:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//your code here
-const todoInput = document.getElementById('newTodoInput');
-const todoAdd = document.getElementById('addTodoBtn');
-const todoList = document.getElementById('todoList');
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.getElementById('newTodoInput');
+    const todoAdd = document.getElementById('addTodoBtn');
+    const todoList = document.getElementById('todoList');
 
-todoAdd.addEventListener('click', () => {
-    const todoText = todoInput.value.trim();
+    todoAdd.addEventListener('click', () => {
+        const todoText = todoInput.value.trim();
 
-    if(todoText) {
-        const todoItem = document.createElement('li');
-        todoItem.textContent = todoText;
-        todoList.appendChild(todoItem);
-		todoInput.value = '';
-    }
-})
+        if (todoText) {
+            const todoItem = document.createElement('li');
+            todoItem.textContent = todoText;
+            todoList.appendChild(todoItem);
+            todoInput.value = '';
+        }
+    });
+});
